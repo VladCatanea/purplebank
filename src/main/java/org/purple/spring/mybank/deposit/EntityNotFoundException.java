d8dd8a3e4e34beb744b04e1bf0a4bb299d1307a0
Vlad <vlad.catanea@yahoo.com>
2024-05-11 10:40:56 +0300

    Added security

--- src/main/java/org/purple/spring/mybank/deposit/EntityNotFoundException.java
+++ src/main/java/org/purple/spring/mybank/deposit/EntityNotFoundException.java
@@ -2,6 +2,11 @@ package org.purple.spring.mybank.deposit;
 
 public class EntityNotFoundException extends RuntimeException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public EntityNotFoundException(Long id) {
 	    super("Could not find entity " + id);
 	  }
